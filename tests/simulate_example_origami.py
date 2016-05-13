318b197cb07e9074313d663e93f1d9fc6682f77b
Alexander Cumberworth <alexandercumberworth@gmail.com>
2016-05-13 10:16:53 +0100

    Fixed imports.

---
+++ tests/simulate_example_origami.py
@@ -0,0 +1,52 @@
+#!/usr/env python
+
+"""Run a basic simulation of the example origami system."""
+
+import sys
+sys.path.append('../')
+from lattice_dna_origami.lattice_origami_domains import *
+
+# Specificy initial configuration by setting input file and step number
+#input_file = JSONInputFile('simple_loop_linear.json')
+#input_file = JSONInputFile('simple_loop.json')
+#input_file = JSONInputFile('cyclic_example.json')
+input_file = JSONInputFile('single_domain.json')
+step = 0
+
+# Set conditions
+temp = 364.5
+
+# Staple strand concentration (M)
+strand_M = 1e-3
+
+# Cation concentration (M)
+cation_M = 1
+
+# Setup origami system object
+#origami_system = OrigamiSystemEight(input_file, step, temp, cation_M)
+origami_system = OrigamiSystemSixteen(input_file, step, temp, strand_M, cation_M)
+
+# Specify moves to be used and associated probabilities
+#move_settings = {MOVETYPE.EXCHANGE_STAPLE: 0.4,
+#                 MOVETYPE.REGROW_STAPLE: 0.2,
+#                 MOVETYPE.REGROW_SCAFFOLD: 0.2,
+#                 MOVETYPE.ROTATE_ORIENTATION_VECTOR: 0.2}
+
+move_settings = {MOVETYPE.EXCHANGE_STAPLE: 1}
+
+# Specify output file type and name
+#output_file_name = 'simple_loop_replica-0.hdf5'
+output_file_name = 'single_domain.hdf5'
+config_write_freq = 0
+count_write_freq = 1
+
+output_file = HDF5OutputFile(output_file_name, origami_system,
+        config_write_freq=config_write_freq,
+        count_write_freq=count_write_freq)
+
+# Setup up simulation
+sim = GCMCSimulation(origami_system, move_settings, output_file)
+
+# Run
+N = 100000
+sim.run(N)
