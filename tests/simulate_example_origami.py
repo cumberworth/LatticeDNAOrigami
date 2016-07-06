59bf58d23441696d48dcde4a037d4690e600ac96
Alexander Cumberworth <alexandercumberworth@gmail.com>
2016-07-06 11:39:43 +0100

    Minor changes to test files.

--- tests/simulate_example_origami.py
+++ tests/simulate_example_origami.py
@@ -9,18 +9,18 @@ from lattice_dna_origami.lattice_origami_domains import *
 # Specificy initial configuration by setting input file and step number
 #input_file = JSONInputFile('simple_loop_linear.json')
 #input_file = JSONInputFile('simple_loop.json')
-#input_file = JSONInputFile('cyclic_example.json')
+input_file = JSONInputFile('cyclic_example.json')
 #input_file = JSONInputFile('single_domain.json')
 #input_file = JSONInputFile('snodin_unbound.json')
 #input_file = JSONInputFile('four_domain_loop.json')
-input_file = JSONInputFile('two_domain.json')
+#input_file = JSONInputFile('two_domain.json')
 step = 0
 
 # Set conditions
-temp = 320
+temp = 350
 
 # Staple strand concentration (M)
-strand_M = 1
+strand_M = 1e-3
 
 # Cation concentration (M)
 #cation_M = 1
@@ -39,16 +39,16 @@ move_settings = {MOVETYPE.EXCHANGE_STAPLE: 0.25,
 
 # Specify output file type and name
 #output_file_name = 'simple_loop_replica-0.hdf5'
-output_file_name = 'four_domain_loop.hdf5'
+output_file_name = 'test.hdf5'
 
 output_file = HDF5OutputFile(output_file_name, origami_system,
-        config_write_freq=1,
+        config_write_freq=10000,
         count_write_freq=0,
-        energy_write_freq=10)
+        energy_write_freq=0)
 
 # Setup up simulation
 sim = GCMCSimulation(origami_system, move_settings, output_file)
 
 # Run
 N = 100000
-sim.run(N, logging=10)
+sim.run(N, logging=1)
