daa09308f70af971b4312943e6c78b3498ebbb74
Alexander Cumberworth <alexandercumberworth@gmail.com>
2016-07-08 16:15:52 +0100

    Speedup by factor of between 2-4, depending on moveset and system.

--- tests/simulate_example_origami.py
+++ tests/simulate_example_origami.py
@@ -9,9 +9,9 @@ from lattice_dna_origami.lattice_origami_domains import *
 # Specificy initial configuration by setting input file and step number
 #input_file = JSONInputFile('simple_loop_linear.json')
 #input_file = JSONInputFile('simple_loop.json')
-input_file = JSONInputFile('cyclic_example.json')
+#input_file = JSONInputFile('cyclic_example.json')
 #input_file = JSONInputFile('single_domain.json')
-#input_file = JSONInputFile('snodin_unbound.json')
+input_file = JSONInputFile('snodin_unbound.json')
 #input_file = JSONInputFile('four_domain_loop.json')
 #input_file = JSONInputFile('two_domain.json')
 step = 0
@@ -31,11 +31,11 @@ cation_M = 1
 origami_system = OrigamiSystemSixteen(input_file, step, temp, strand_M, cation_M)
 
 # Specify moves to be used and associated probabilities
-move_settings = {MOVETYPE.EXCHANGE_STAPLE: 0.25,
-                 MOVETYPE.REGROW_STAPLE: 0.25,
-                 MOVETYPE.CB_REGROW_SCAFFOLD: 0.25,
-                 #MOVETYPE.CB_CONSERVED_TOPOLOGY: 0.25,
-                 MOVETYPE.ROTATE_ORIENTATION_VECTOR: 0.25}
+move_settings = {MOVETYPE.EXCHANGE_STAPLE: 1/4,
+                 MOVETYPE.CB_REGROW_STAPLE: 1/4,
+                 MOVETYPE.CB_REGROW_SCAFFOLD: 1/4,
+                 #MOVETYPE.CB_CONSERVED_TOPOLOGY: 1/4,
+                 MOVETYPE.ROTATE_ORIENTATION_VECTOR: 1/4}
 
 # Specify output file type and name
 #output_file_name = 'simple_loop_replica-0.hdf5'
@@ -47,8 +47,8 @@ output_file = HDF5OutputFile(output_file_name, origami_system,
         energy_write_freq=0)
 
 # Setup up simulation
-sim = GCMCSimulation(origami_system, move_settings, output_file)
+sim = GCMCSimulation(origami_system, move_settings, output_file, center_freq=10000)
 
 # Run
 N = 100000
-sim.run(N, logging=1)
+sim.run(N, logging=100)
