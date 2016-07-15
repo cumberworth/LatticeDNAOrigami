58523ab9da805ee6a4bbd66820747890523904bf
Alexander Cumberworth <alexandercumberworth@gmail.com>
2016-07-15 17:48:03 +0100

    Fixed problem with energies.

--- tests/simulate_example_origami.py
+++ tests/simulate_example_origami.py
@@ -9,11 +9,11 @@ from lattice_dna_origami.lattice_origami_domains import *
 # Specificy initial configuration by setting input file and step number
 #input_file = JSONInputFile('simple_loop_linear.json')
 #input_file = JSONInputFile('simple_loop.json')
-input_file = JSONInputFile('cyclic_example.json')
+#input_file = JSONInputFile('cyclic_example.json')
 #input_file = JSONInputFile('single_domain.json')
 #input_file = JSONInputFile('snodin_unbound.json')
 #input_file = JSONInputFile('four_domain_loop.json')
-#input_file = JSONInputFile('two_domain.json')
+input_file = JSONInputFile('two_domain.json')
 step = 0
 
 # Set conditions
@@ -31,12 +31,12 @@ cation_M = 1
 origami_system = OrigamiSystemSixteen(input_file, step, temp, strand_M, cation_M, misbinding=False)
 
 # Specify moves to be used and associated probabilities
-move_settings = {MOVETYPE.EXCHANGE_STAPLE: 1/4,
+move_settings = {MOVETYPE.EXCHANGE_STAPLE: 1/3,
                  #MOVETYPE.IDENTITY: 1}
-                 MOVETYPE.REGROW_STAPLE: 1/4,
-                 MOVETYPE.REGROW_SCAFFOLD: 1/4,
+                 MOVETYPE.REGROW_STAPLE: 1/3,
+                 #MOVETYPE.REGROW_SCAFFOLD: 1/4,
                  #MOVETYPE.CB_CONSERVED_TOPOLOGY: 1/4,
-                 MOVETYPE.ROTATE_ORIENTATION_VECTOR: 1/4}
+                 MOVETYPE.ROTATE_ORIENTATION_VECTOR: 1/3}
 
 # Specify output file type and name
 #output_file_name = 'simple_loop_replica-0.hdf5'
