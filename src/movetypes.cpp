a18a114f9da7ea3c9be22ee2e6d12ea99c4bb602
Alexander Cumberworth <alexandercumberworth@gmail.com>
2016-10-18 18:44:43 +0100

    Purposely removing overcount from both staple regrowths.

--- src/movetypes.cpp
+++ src/movetypes.cpp
@@ -452,6 +452,8 @@ bool MetStapleRegrowthMCMovetype::attempt_move() {
     }
 
     double boltz_factor {exp(-m_delta_e)};
+    // DEGUB
+    m_modifier = 1;
     accepted = test_acceptance(boltz_factor);
     return accepted;
 }
@@ -892,7 +894,8 @@ bool CBStapleRegrowthMCMovetype::attempt_move() {
     set_growthpoint_and_grow_staple(growthpoint, selected_chain);
 
     // Revert modifier and test acceptance
-    m_modifier = m_new_modifier;
+    //m_modifier = m_new_modifier;
+    m_modifier = 1;
     accepted = test_cb_acceptance();
     return accepted;
 }
