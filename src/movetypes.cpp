90061d00f28e98b76c6c78b1a8dad6aa6396fa66
Alexander Cumberworth <alexandercumberworth@gmail.com>
2016-11-01 19:47:38 +0000

    Removed block on number of staples

--- src/movetypes.cpp
+++ src/movetypes.cpp
@@ -347,9 +347,9 @@ bool MetStapleExchangeMCMovetype::insert_staple() {
     int c_i_ident {select_random_staple_identity()};
 
     //DEBUG
-    if (m_origami_system.num_staples_of_ident(c_i_ident) == 2) {
-        return false;
-    }
+    //if (m_origami_system.num_staples_of_ident(c_i_ident) == 2) {
+    //    return false;
+    // }
 
     int c_i {m_origami_system.add_chain(c_i_ident)};
     m_added_chains.push_back(c_i);
