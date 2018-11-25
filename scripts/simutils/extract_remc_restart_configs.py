1efe5efafaf6748215b3118f0b45067e3af9aa90
Alexander <alexandercumberworth@gmail.com>
2018-11-25 18:53:44 +0000

    Merging in changes from modernized python code.
    Also changes to allow specifying which reps to use in MBAR analysis.

---
+++ scripts/simutils/extract_remc_restart_configs.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""Extract final configurations for restarting REMC runs."""
+
+import argparse
+import pdb
+import sys
+
+from origamipy.io import *
+
+
+def main():
+    args = parse_args()
+    swap_inp_filename = '{}.swp'.format(args.inp_filebase)
+    swap_file = UnparsedSingleLineStepInpFile(swap_inp_filename, headerlines=1)
+    swap_out_filename = '{}.swp.restart'.format(args.out_filebase)
+    with open(swap_out_filename, 'w') as out:
+        out.write(swap_file.header)
+        out.write(swap_file.get_last_step())
+
+    for thread in range(args.threads):
+        traj_inp_filename = '{}-{}.trj'.format(args.inp_filebase, thread)
+        traj_file = UnparsedMultiLineStepInpFile(traj_inp_filename)
+        traj_out_filename = '{}-{}.trj.restart'.format(args.out_filebase, thread)
+        with open(traj_out_filename, 'w') as out:
+            out.write(traj_file.get_last_step())
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'inp_filebase',
+        type=str,
+        help='Input filename')
+    parser.add_argument(
+        'out_filebase',
+        type=str,
+        help='Output filename')
+    parser.add_argument(
+        'threads',
+        type=int,
+        help='Number of threads/replicas')
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
