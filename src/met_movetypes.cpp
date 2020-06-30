b7def8e1a4b6169a64cafe8838f816a498289d7f
Alexander <alexandercumberworth@gmail.com>
2020-06-30 18:13:34 +0200

    Int type caused invalid probabilty.

--- src/met_movetypes.cpp
+++ src/met_movetypes.cpp
@@ -216,7 +216,7 @@ bool MetStapleExchangeMCMovetype::staple_insertion_accepted(
     double boltz_factor {exp(-m_delta_e)};
     int Ni_new {m_origami_system.num_staples_of_ident(c_i_ident)};
     size_t staple_length {m_origami_system.m_identities[c_i_ident].size()};
-    double pratio {static_cast<double>(staple_length) * 6 / Ni_new * boltz_factor};
+    double pratio {static_cast<double>(staple_length) * 6.0 / Ni_new * boltz_factor};
     pratio *= m_insertion_sites * m_origami_system.m_reduced_fugacity;
     pratio /= num_staple_bd;
 
@@ -261,7 +261,7 @@ bool MetStapleExchangeMCMovetype::staple_deletion_accepted(
     double boltz_factor {exp(-m_delta_e)};
     int Ni {m_origami_system.num_staples_of_ident(c_i_ident)};
     size_t staple_length {m_origami_system.m_identities[c_i_ident].size()};
-    double pratio {Ni / 6 / static_cast<double>(staple_length) * boltz_factor};
+    double pratio {Ni / 6.0 / static_cast<double>(staple_length) * boltz_factor};
     pratio /= m_origami_system.m_reduced_fugacity *
               (m_insertion_sites - staple_length);
     pratio *= num_staple_bd;
@@ -378,7 +378,7 @@ bool MetStapleExchangeMCMovetype::delete_staple() {
     unassign_domains(staple);
 
     // Hack
-    m_delta_e += log(6);
+    m_delta_e -= log(6);
     accepted = staple_deletion_accepted(c_i_ident, num_staple_bd);
     if (accepted) {
         m_origami_system.delete_chain(c_i);
