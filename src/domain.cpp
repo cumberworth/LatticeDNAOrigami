f56cca1a416fbc88dcac5a43eb27fec380f7df98
Alexander <alexandercumberworth@gmail.com>
2020-08-20 11:55:13 +0200

    Removed redundant checks.

--- src/domain.cpp
+++ src/domain.cpp
@@ -109,42 +109,13 @@ bool ThreeQuarterTurnDomain::check_junction_constraint(
     VectorThree ndr_k1 {cd_k2.m_pos - cd_k1.m_pos};
     if (ndr_k1 == cd_k1.m_ore) {
         VectorThree ndr_1 {cd_j2.m_pos - this->m_pos};
-        VectorThree ndr_3 {cd_j4.m_pos - cd_j3.m_pos};
-        bool pair1_stacked {false};
-        if (ndr_1 != this->m_ore and ndr_1 != -this->m_ore) {
-            pair1_stacked = this->check_twist_constraint(ndr_1, cd_j2);
+        if (this->m_d > cd_j2.m_d) {
+            ndr_1 = -ndr_1;
         }
-        if (pair1_stacked) {
-            bool pair2_stacked {false};
-            if (ndr_3 != cd_j3.m_ore and ndr_3 != -cd_j3.m_ore) {
-                pair2_stacked = cd_j3.check_twist_constraint(ndr_3, cd_j4);
-            }
-            if (pair2_stacked) {
-                if (this->m_d > cd_j2.m_d) {
-                    ndr_1 = -ndr_1;
-                }
-                if (not cd_k1.check_twist_constraint(ndr_1, cd_k2)) {
-                    kink_constraint_obeyed = false;
-                }
-            }
+        if (not cd_k1.check_twist_constraint(ndr_1, cd_k2)) {
+            kink_constraint_obeyed = false;
         }
     }
     return kink_constraint_obeyed;
-}
-
-bool check_pair_stacked(Domain* cd_1, Domain* cd_2) {
-
-    bool stacked {false};
-    if (cd_1->m_d > cd_2->m_d) {
-        Domain* hold {cd_1};
-        cd_1 = cd_2;
-        cd_2 = hold;
-    }
-    VectorThree ndr {cd_2->m_pos - cd_1->m_pos};
-    if (ndr != cd_1->m_ore and ndr != -cd_1->m_ore) {
-        stacked = cd_1->check_twist_constraint(ndr, *cd_2);
-    }
-
-    return stacked;
 }
 } // namespace domainContainer
